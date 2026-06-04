e4e42d4c95573fe5f1855a95926fbe8dd9c93450
harinandandeloitte <harinandangariya@gmail.com>
2026-06-05 00:44:06 +0530

    second time updated code

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,7 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
-    parent_variable='parent_variable';
+    parent_variable='parent_variable is done';
     
     
 }
